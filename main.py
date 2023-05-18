094f144bc3bad102d8d75d6f80538b5ac40666af
Momir Stanišić <momir.stanisic.ns@gmail.com>
2023-05-18 18:21:09 +0200

    Added score

--- main.py
+++ main.py
@@ -19,11 +19,14 @@ EMPTY = bcolors.OKGREEN + " ◇"
 BLACK = bcolors.OKBLUE + " ◉"
 WHITE = bcolors.ENDC + " ◉"
 MOVES = bcolors.WARNING + " ◎"
-SCORE_W = 0
-SCORE_B = 0
+ROBOT_ICON = '󱚝'  # 󱚞 󰚩
+USER_ICON = '󰀄'  # 󰀉
+PLAYER_W = ROBOT_ICON
+PLAYER_B = USER_ICON
 SCORE_HEIGHT = 64
-WIDTH = 800
+SCORE = {WHITE: 2, BLACK: 2}
 HEIGHT = 800 + SCORE_HEIGHT
+WIDTH = 800
 T_BORDER = 64 + SCORE_HEIGHT
 B_BORDER = 64
 L_BORDER = 64
@@ -38,10 +41,13 @@ BORDER_POSITIONS = ((-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1)
 # često korišćene vrednosti
 T_WIDTH = WIDTH - L_BORDER - R_BORDER
 T_HEIGHT = HEIGHT - T_BORDER - B_BORDER
-RADIUS = int(min(T_WIDTH, T_HEIGHT) / 20)
+RADIUS = min(T_WIDTH, T_HEIGHT) // 20
 
 
 pygame.init()
+font_mdi = pygame.font.Font('mdi.ttf', 64)
+font_roboto = pygame.font.Font('roboto.ttf', 64)
+font_roboto2 = pygame.font.Font('roboto.ttf', 24)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 programIcon = pygame.image.load('icon.png')
 pygame.display.set_icon(programIcon)
@@ -84,9 +90,19 @@ def draw_piece(x, y, color):
         gfxdraw.filled_circle(screen, x, y, RADIUS - 1, GREEN_C)
         gfxdraw.aacircle(screen, x, y, RADIUS - 1, GREEN_C)
 
+def set_score():
+    player_b = font_mdi.render(USER_ICON, True, WHITE_C)
+    player_w = font_mdi.render(ROBOT_ICON, True, WHITE_C)
+    b_box = player_b.get_rect()
+    w_box = player_w.get_rect()
+    b_box.midleft = (L_BORDER, SCORE_HEIGHT)
+    w_box.midright = (WIDTH - R_BORDER, SCORE_HEIGHT)
+    screen.blit(player_b, b_box)
+    screen.blit(player_w, w_box)
 
 def set_board():
-    pygame.draw.rect(screen, BACKGROUND_C, pygame.Rect(0, 0, WIDTH, HEIGHT))
+    screen.fill(BACKGROUND_C)
+    set_score()
     pygame.draw.rect(screen, GREEN_C, pygame.Rect(L_BORDER, T_BORDER, WIDTH - L_BORDER - R_BORDER, HEIGHT - T_BORDER - B_BORDER))
     for i in range(9):
         pygame.draw.line(screen, BLACK_C, (L_BORDER + T_WIDTH * i / 8, T_BORDER), (L_BORDER + T_WIDTH * i / 8, HEIGHT - B_BORDER))
@@ -99,8 +115,29 @@ def set_board():
         board[3 + i][3 + i] = WHITE
         board[3 + i][4 - i] = BLACK
 
+def print_game_over():
+    text1 = font_roboto2.render('GAME', True, WHITE_C, BACKGROUND_C)
+    text2 = font_roboto2.render('OVER', True, WHITE_C, BACKGROUND_C)
+    box1 = text1.get_rect()
+    box2 = text2.get_rect()
+    box1.midbottom = (WIDTH // 2, SCORE_HEIGHT)
+    box2.midtop = (WIDTH // 2, SCORE_HEIGHT)
+    screen.blit(text1, box1)
+    screen.blit(text2, box2)
+
+def print_score():
+    score_b = font_roboto.render(str(SCORE[BLACK]), True, WHITE_C, BACKGROUND_C)
+    score_w = font_roboto.render(str(SCORE[WHITE]), True, WHITE_C, BACKGROUND_C)
+    b_box = score_b.get_rect()
+    w_box = score_w.get_rect()
+    b_box.midleft = (L_BORDER * 2.5, SCORE_HEIGHT)
+    w_box.midright = (WIDTH - R_BORDER * 2.5, SCORE_HEIGHT)
+    screen.blit(score_b, b_box)
+    screen.blit(score_w, w_box)
+
 def print_board():
     log_board()
+    print_score()
     for x in range(8):
         for y in range(8):
             draw_piece(x, y, board[x][y])
@@ -141,6 +178,8 @@ def add_moves(player):
 def flip_line(player, opponent, x, y, xi, yi):
     while board[x][y] == opponent:
         board[x][y] = player
+        SCORE[opponent] -= 1
+        SCORE[player] += 1
         x, y = x + xi, y + yi
 
 def flip(player, x, y):
@@ -163,6 +202,7 @@ while True:
             if pos[0] > L_BORDER and pos[0] < WIDTH - R_BORDER and pos[1] > T_BORDER and pos[1] < HEIGHT - B_BORDER:
                 x, y = xy_to_pos(pos[0], pos[1])
                 if board[x][y] == MOVES:
+                    SCORE[trenutni] += 1
                     board[x][y] = trenutni
                     flip(trenutni, x, y)
                     trenutni = WHITE if trenutni == BLACK else BLACK
@@ -170,10 +210,9 @@ while True:
                     print_board()
                     if not has_moves:
                         trenutni = WHITE if trenutni == BLACK else BLACK
-                        add_moves(trenutni)
+                        has_moves = add_moves(trenutni)
                         print_board()
-
-
-
+                        if not has_moves:
+                            print_game_over()
 
     pygame.display.update()
