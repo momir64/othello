801039be0195c897d5363e5443496fbdda85a091
Momir Stanišić <momir.stanisic.ns@gmail.com>
2023-05-22 22:26:06 +0200

    Commenting

--- globals.py
+++ globals.py
@@ -6,5 +6,6 @@ WHITE = 2
 MOVES = 3
 
 # random određivanje da li korisnik igra prvi ili drugi
+# prvi igrač je uvek crni
 USER = BLACK if random.getrandbits(1) else WHITE
 BOT = WHITE if USER == BLACK else BLACK
